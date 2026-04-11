7b8c8b20dbdcdbb8e4120e259095a28cc91dde02
Lucas Eduardo Pereira da Cruz <lucascruzjau@gmail.com>
2026-04-10 23:21:03 -0300

    Terminando os indices

--- Aula05-indices/indices.js
+++ Aula05-indices/indices.js
@@ -51,3 +51,43 @@ db.usuarios.getIndexes()
 // Continuação de códigos mais "Complexos" // 
 // Oq falta ainda: Detalhando o espaço usado por indices de uma coleção, como saber se o indice está sendo usado, 
 // Para oq serve o hint() e exemplo de uso do mesmo, avaliando desempenhos de indices, como usar o .Explain()
+
+
+// ====== ESPAÇO USADO POR INDICES DE UMA COLEÇÃO ======= //
+db.usuarios.stats().indexSizes
+
+// Saída esperada 
+{
+    "_id_": 4096000,
+    "email_1": 1146880,
+    "_nome_1_idade_1": 2293760
+}
+
+// Explicação 
+ // _id_ 4mb (Mongo cria esse indice automaticamente)
+ // email_1 1.1mb indice no campo email 
+ // nome_1_idade_1 2.2 indice composto em nome e idade 
+
+
+
+// ====== COMO SABER SE O INDICE ESTÁ SENDO USADO ======= //
+ // Podemos analisar como uma consulta está sendo processada com .explain("executionStats")
+
+// Exemplo sem indice 
+db.usuarios.find({email: "joao@email.com"}).explain("executionStats") 
+
+// Exemplo com indice 
+db.usuarios.find({email: "joao@email.com"}).hint({email: 1}).explain("executionStats")
+
+ // Se o totalKeysExamined e totalDocsExamined for baixo, foi utilizado com sucesso 
+
+
+
+
+// ====== EXEMPLO USANDO HINT ======= //
+db.pedidos.createIndex({email: 1})
+db.pedidos.createIndex({cliente: 1, status: 1})
+
+db.pedidos.find({email: "joao@email.com"}).explain("executionStats")
+
+db.pedidos.find({email: "joao@email.com"}).hint({cliente: 1, status: 1}).explain("executionStats")
