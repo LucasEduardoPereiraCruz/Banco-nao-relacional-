e4d1ff568d43f5213a70cacbe65d442c8262772a
Lucas Eduardo Pereira da Cruz <lucascruzjau@gmail.com>
2026-04-20 22:49:10 -0300

    Add o campo Sexo em proprietários

--- Aula06-15-04/transformacao_proprietario.js
+++ Aula06-15-04/transformacao_proprietario.js
@@ -1,47 +1,57 @@
 db.pessoas_rascunho.aggregate([
     {
         $project: {
-            _id: 0, 
-            nome: 1, 
+            _id: 0,
+            nome: 1,
 
             cpf: {
                 $concat: [
-                    { $toString: { $floor: { $multiply: [{$rand: {}}, 1000] } } },
-                ".",    
-                    { $toString: { $floor: { $multiply: [{$rand: {}}, 1000] } } },
-                ".",
-                    { $toString: { $floor: { $multiply: [{$rand: {}}, 1000] } } },
-                "-",
-                    { $toString: { $floor: { $multiply: [{$rand: {}}, 100] } } }
+                    { $toString: { $floor: { $multiply: [{ $rand: {} }, 1000] } } },
+                    ".",
+                    { $toString: { $floor: { $multiply: [{ $rand: {} }, 1000] } } },
+                    ".",
+                    { $toString: { $floor: { $multiply: [{ $rand: {} }, 1000] } } },
+                    "-",
+                    { $toString: { $floor: { $multiply: [{ $rand: {} }, 100] } } }
                 ]
             }
-        }, 
+        },
+
+        sexo: {
+            $arrayElemAt: [
+                ["Masculino", "Feminino"],
+                { $floor: { $multiply: [{ $rand: {} }, 2] } }
+            ]
+        },
+
         endereco: {
-            logradouro: "Rua exemplo", 
-            numero: {$floor: {$multiply: [{rand: {}}, 1000] } }, 
-            bairro: "Centro", 
-            cidade: "Jaú", 
+            logradouro: "Rua exemplo",
+            numero: { $floor: { $multiply: [{ rand: {} }, 1000] } },
+            bairro: "Centro",
+            cidade: "Jaú",
             cep: {
                 $concat: [
-                    { $toString: { $floor: { $multiply: [{$rand: {}}, 100000] } } },
+                    { $toString: { $floor: { $multiply: [{ $rand: {} }, 100000] } } },
                     "-",
-                    { $toString: { $floor: { $multiply: [{$rand: {}}, 1000] } } }
+                    { $toString: { $floor: { $multiply: [{ $rand: {} }, 1000] } } }
                 ]
             }
-        }, 
+        },
 
         cadastro: {
             $add: [
-                new Date("2026-01-01"), 
-                {$multiply: [
-                    {$rand: {} }, 
-                    (new Date() - new Date("2026-01-01"))
-                ]}
+                new Date("2026-01-01"),
+                {
+                    $multiply: [
+                        { $rand: {} },
+                        (new Date() - new Date("2026-01-01"))
+                    ]
+                }
             ]
         }
 
     },
 
     { $limit: 10 },
-    {$out: "Proprietário"}
+    { $out: "Proprietário" }
 ]); 
